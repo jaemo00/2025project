b2572c5a5045043082a58477d43b5fec93ae901e
김재모 <rmramm7@naver.com>
2025-04-29 02:42:31 +0900

    web socket and UUID

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -14,5 +14,35 @@
       ScenarioSection,
       ArrowNextButton,
     },
+    data() {
+      return {
+        userId: null,
+       socket: null
+      };
+    },
+    mounted() {
+    // UUID 생성
+      this.userId = crypto.randomUUID();
+      console.log("생성된 사용자 ID:", this.userId);
+
+     // 웹소켓 연결하면서 id전달
+      this.socket = new WebSocket(`ws://localhost:8000/ws?user_id=${this.userId}`);
+
+     this.socket.onopen = (event) => {
+      console.log("웹소켓 연결 성공");
+      };
+
+      this.socket.onclose = (event) => {
+        console.log("웹소켓 연결 종료");
+     };
+
+      this.socket.onerror = (error) => {
+       console.error("웹소켓 에러 발생:", error);
+     };
+
+      this.socket.onmessage = (event) => {
+       console.log("서버로부터 받은 메시지:", event.data);
+      };
+    }
   };
   </script>
